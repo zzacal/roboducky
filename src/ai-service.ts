34d9136021e06f8daf00de82d146bf884b11c74e
zzacal <jizacal@gmail.com>
2023-04-01 23:09:24 -0700

    Integrates openai chat service

--- src/ai-service.ts
+++ src/ai-service.ts
@@ -1,4 +1,4 @@
-import { openaiClient } from "./openai";
+import { openaiClient } from "./ai-clients";
 
 export type ChatHandler = (prompt: string) => Promise<string>;
 
@@ -11,10 +11,12 @@ export type Command = {
 export const ChatGptCommand: Command = {
   name: "ChatGpt3",
   description: "openai chat gpt 3.5",
-  handler: function (prompt: string): Promise<string> {
-    openaiClient.createChatCompletion({
-      model: "",
-      messages: []
+  handler: async function (prompt: string) {
+    const result = await openaiClient.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: prompt}],
+      temperature: 0.7
     })
+    return result.data.choices.map(c => c.message?.content).join("\n") ?? "";
   }
 }
